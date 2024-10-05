1e04c1ae3e28f85d551b5b5f735e76ec5077c98f
Black Roland <mail@roland.black>
2024-10-05 22:26:55 +0300

    Parse journalctl output instead of using sd-journal
    Crosscompilation is not working properly for libsystemd.

--- src/direwolf.rs
+++ src/direwolf.rs
@@ -1,21 +1,20 @@
-use sd_journal::Journal;
-use chrono;
+use std::process::Command;
+use regex::Regex;
 
 pub fn get_sound_level() -> i32 {
-  let journal = Journal::open_files(["system@6f88fa98141b4f26a9ffc8439e1f2388-000000000005ad1c-000623a4a1eeb386.journal"]).unwrap();
+  let journalctl_out = Command::new("journalctl")
+    .args(&["-o", "cat", "SYSLOG_IDENTIFIER=direwolf", "--since", "65 seconds ago", "--reverse"])
+    .output()
+    .expect("Failed to execute journalctl");
 
-  let (_start, end) = journal.get_realtime_cutoff().unwrap();
-  let since = end
-      .checked_sub_signed(chrono::Duration::seconds(65))
-      .unwrap();
-  journal.seek_realtime(since).unwrap();
+  let stdout = String::from_utf8_lossy(&journalctl_out.stdout);
+  let level_re = Regex::new(r"(?m)receive audio level CH0 ([0-9]{1,3})$").unwrap();
 
-  journal.add_match("SYSLOG_IDENTIFIER=direwolf").unwrap();
+  let Some(captures) = level_re.captures(&stdout) else {
+    return 0;
+  };
 
-  // journal.seek_tail().unwrap();
-  journal.next().unwrap();
-
-  println!("{}", journal.get_data("MESSAGE").unwrap());
-
-  0
+  captures
+    .get(1)
+    .map_or(0, |m| m.as_str().parse::<i32>().unwrap())
 }
